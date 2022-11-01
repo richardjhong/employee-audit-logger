735aa9a7a6bdb6419b76160c200c38ba48917f60
Richard Hong <rhong24@gmail.com>
2022-10-31 19:01:45 -0500

    using npm dotenv and changing config/connections to listen to env var for db connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const db = mysql.createConnection(
   {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'employee_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   },
   console.log('Connection to employee_db established')
 );
